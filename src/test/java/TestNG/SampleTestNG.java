4c1dd8ca84940430eede7163872a500712af5a56
anilc <anilc@AnilPC>
2025-07-18 14:17:09 +0530

    Commit2

--- src/test/java/TestNG/SampleTestNG.java
+++ src/test/java/TestNG/SampleTestNG.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class SampleTestNG {
 //sAMPLE cHANGE fROM gITHUB
 	@Test
-	
+//sample change and push to github
 	public void test() {
 		System.out.println("Testing");
 	}
